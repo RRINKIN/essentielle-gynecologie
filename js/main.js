36845ef30c742eab272ec9abab5f05ad3692498c
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-09-04 20:00:37 +0200

    update everything

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function openThankU(event){
         .catch(error => console.log(error));
 }
 
-// Close thank u popup
+// Close "thank u" popup
 function closeThankU(){
     thankU.classList.remove("openPopup");
     main.classList.remove("blur");
@@ -71,3 +71,24 @@ function closeContact(){
     header.classList.remove("blur");
     footer.classList.remove("blur");
 }
+
+// onScroll animations of the expertness bar
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+      addObserver(el, options)
+    })
+}
+function addObserver(el, options) {
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('active')
+          observer.unobserve(entry.target)
+        }
+      })
+    }, options) // Passing the options object to the observer
+    observer.observe(el)
+  }
+scrollTrigger('.l-main-section-expertnessBar-inside')
